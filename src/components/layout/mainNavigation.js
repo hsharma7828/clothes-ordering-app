0dbae5f89f06e4cbcf775f20f8233e8a99418018
Himanshu Sharma~ <hsharma114@gmail.com>
2023-07-04 15:43:32 +0530

    React Bootstarp setup for the application

--- src/components/layout/mainNavigation.js
+++ src/components/layout/mainNavigation.js
@@ -1,26 +1,33 @@
-import { NavLink } from "react-router-dom";
-import classes from "./mainNavigation.module.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function MainNavigation() {
   return (
-    <header>
-      <nav>
-        <ul className={classes.unOrderesList}>
-          <NavLink className={classes.listItem} to={"/"}>
-            Home
-          </NavLink>
-          <NavLink className={classes.listItem} to={"/clothes"}>
-            Clothes
-          </NavLink>
-          {/* <li>
-            <NavLink to={}>Contact</NavLink>
-          </li>
-          <li>
-            <NavLink to={}>About</NavLink>
-          </li> */}
-        </ul>
-      </nav>
-    </header>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 

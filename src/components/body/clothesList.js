2865eac2307bf76485c4510d7bff5b506ba6e93e
Himanshu Sharma~ <hsharma114@gmail.com>
2023-07-30 22:31:06 +0530

    some fixes for data filter & filter icon perforation

--- src/components/body/clothesList.js
+++ src/components/body/clothesList.js
@@ -5,9 +5,35 @@ import { useState } from "react";
 
 function ClothesList({ events }) {
   const [openList, setOpenList] = useState(false);
+  const [productList, setProductList] = useState([...events[0]]);
   function setOpenListHandler() {
     setOpenList(!openList);
   }
+  function filterListHandler(value) {
+    setProductList(filterProductList([...events[0]], value));
+  }
+  function filterProductList(list, value) {
+    let listData = [];
+    list.forEach((ele) => {
+      if (ele.category === value) {
+        listData.push(ele);
+      }
+    });
+    return listData;
+  }
+  const dataList = (
+    <ul className={classes.list}>
+      {productList.map((event) => (
+        <li key={event.id} className={classes.item}>
+          <Link to={`/products/${event.id}`}>
+            <h2>{event.title}</h2>
+            <img src={event.image} alt={event.title} />
+            <div className={classes.content}></div>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
   return (
     <div className={classes.events}>
       <h1>All Available Products</h1>
@@ -18,26 +44,16 @@ function ClothesList({ events }) {
           className={classes.filter}
           onClick={setOpenListHandler}
         />
-        {openList && (
-          <ul className={classes.filterList}>
-            {events[1].map((ele, index) => (
-              <li>{ele}</li>
-            ))}
-          </ul>
-        )}
+        <ul className={openList ? classes.filterList : classes.hideul}>
+          {events[1].map((ele) => (
+            <li value={ele} onClick={() => filterListHandler(ele)}>
+              {ele}
+            </li>
+          ))}
+        </ul>
       </div>
       <hr></hr>
-      <ul className={classes.list}>
-        {events[0].map((event) => (
-          <li key={event.id} className={classes.item}>
-            <Link to={`/products/${event.id}`}>
-              <h2>{event.title}</h2>
-              <img src={event.image} alt={event.title} />
-              <div className={classes.content}></div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {dataList}
     </div>
   );
 }
